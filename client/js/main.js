98cf519d08f2f71caa112d2bbf7531a594c6033e
Sanjay2506 <106942393+sanjaysekar2506@users.noreply.github.com>
2025-01-16 17:31:41 +0530

    Update main.js

--- client/js/main.js
+++ client/js/main.js
@@ -117,7 +117,7 @@
         }
     });
 
-    const apiUrl = 'http://localhost:5000'; // Make sure this is the correct API URL
+    const apiUrl = 'https://sbt-api.vercel.app'; // Make sure this is the correct API URL
 
     // Function to render the products dynamically based on the category selected
     function renderProducts(products) {
